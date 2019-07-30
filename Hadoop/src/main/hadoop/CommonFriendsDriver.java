43df4221b22bdd67cb9366d321fb66160af9a166
hanj <jie_han163@163.com>
2019-07-30 10:43:02 +0800

    hadoop 求共同好友

--- Hadoop/src/main/hadoop/CommonFriendsDriver.java
+++ Hadoop/src/main/hadoop/CommonFriendsDriver.java
@@ -1,9 +1,12 @@
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -14,6 +17,7 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  * @author: hanj
@@ -31,24 +35,35 @@ public class CommonFriendsDriver extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
+        Configuration conf = new Configuration();
 
-        Job job = Job.getInstance();
-        job.setJobName("");
+        Job job = Job.getInstance(conf);
+        job.setJobName("CommonFriendsDriver");
         job.setJarByClass(CommonFriendsDriver.class);
 
 //        HadoopUtil.addJarsToDistributedCache(job,"/lib/");
 
+        String input = "G:\\data\\hadoop\\CommonFriends.txt";
+        String output = "G:\\data\\hadoop\\CommonFriends";
+
+        Path inputPath = new Path(input);
+        Path outputpath = new Path(output);
+
+
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
+
+        //如果文件存在就删除
+        FileSystem.get(getConf()).delete(outputpath,true);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        job.setMapperClass();
-        job.setReducerClass();
+        job.setMapperClass(CommonFriendsMapper.class);
+        job.setReducerClass(CommonFriendsReducer.class);
 
-        FileInputFormat.setInputPaths(job,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        FileInputFormat.setInputPaths(job,inputPath);
+        FileOutputFormat.setOutputPath(job,outputpath);
 
         boolean status = job.waitForCompletion(true);
         theLogger.info("run():status= "+status);
@@ -70,9 +85,9 @@ class CommonFriendsMapper extends Mapper<LongWritable,Text,Text,Text>{
     private static final Text REDUCE_VALUE = new Text();
 
     static String getFriends(String[] tokens){
-        if (tokens.length == 2){
-            return "";
-        }
+//        if (tokens.length == 2){
+//            return "";
+//        }
         StringBuilder builder = new StringBuilder();
         for (int i = 1; i<tokens.length; i++){
             builder.append(tokens[i]);
@@ -95,9 +110,9 @@ class CommonFriendsMapper extends Mapper<LongWritable,Text,Text,Text>{
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] tokens = StringUtils.split(value.toString(),',');
-        System.out.println("tokens= "+tokens);
+//        System.out.println("tokens= "+Arrays.toString(tokens));
         String fridens = getFriends(tokens);
-        System.out.println("friend= "+fridens);
+//        System.out.println("friend= "+fridens);
         REDUCE_VALUE.set(fridens);
         String person = tokens[0];
         for (int i=1;i<tokens.length;i++){
@@ -107,6 +122,48 @@ class CommonFriendsMapper extends Mapper<LongWritable,Text,Text,Text>{
             context.write(REDUCE_KEY,REDUCE_VALUE);
         }
     }
+}
 
+class CommonFriendsReducer extends Reducer<Text,Text,Text,Text> {
+
+    @Override
+    public void reduce(Text key,Iterable<Text> values,Context context) throws IOException, InterruptedException {
+        System.out.println("key= "+key.toString());
+        Map<String,Integer> map = new HashMap<String, Integer>();
+        Iterator<Text> iterator = values.iterator();
+        int numOfValues = 0;
+        while (iterator.hasNext()){
+            String friends = iterator.next().toString();
+            System.out.println("friends = "+friends);
+            if (friends.equals("")){
+                context.write(key,new Text("[]"));
+            }
+            addFriends(map,friends);
+            numOfValues++;
+        }
+        List<String> commonFriends = new ArrayList<>();
+        for (Map.Entry<String,Integer> entry: map.entrySet()){
+            if (entry.getValue() == numOfValues){
+                commonFriends.add(entry.getKey());
+            }
+        }
+        context.write(key,new Text(commonFriends.toString()));
+    }
+
+    static void addFriends(Map<String, Integer> map, String friendsList) {
+        String[] friends = StringUtils.split(friendsList,',');
+        for (String friend : friends){
+            Integer count = map.get(friend);
+            if (count == null){
+                map.put(friend,1);
+            }else {
+                map.put(friend,++count);
+            }
+        }
+    }
 
 }
+
+
+
+
